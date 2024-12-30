be4a6d85891981d9a44c051184406e14145c2534
Jason Pohl <jsnpohl@gmail.com>
2024-12-30 13:37:05 -0700

    updates form factor awareness

--- force-app/main/default/lwc/notification/notification.js
+++ force-app/main/default/lwc/notification/notification.js
@@ -1,22 +1,24 @@
 import { LightningElement, api } from 'lwc';
+import FORM_FACTOR from '@salesforce/client/formFactor';
 
 export default class Notification extends LightningElement {
     showNotification = false;
     variant
     message
 
-    
     get notificationClasses() {
         let variantClass = this.variant === 'success' ? 'success' : this.variant === 'error' ? 'error' : 'info';
         return `slds-notify slds-notify_toast notification-shared-styles ${variantClass}`;
     }
     @api
     showToast(message, variant) {
-        this.variant = variant || 'info';
-        this.message = message || 'Notification Message goes here';
-        this.showNotification = true;
-        setTimeout(()=>{
-            this.showNotification = false;
-        }, 4000)
+        if(FORM_FACTOR == 'Large') {
+            this.variant = variant || 'info';
+            this.message = message || 'Notification Message goes here';
+            this.showNotification = true;
+            setTimeout(()=>{
+                this.showNotification = false;
+            }, 4000)
+        }  
     }
 }
